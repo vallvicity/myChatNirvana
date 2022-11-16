55f1ed60309f5d814a8add0bf6b618841e29849c
Albert Ramos <vallvicity@hotmail.com>
2022-11-16 13:52:46 +0100

    Tercer commit

--- src/chat/Chat.jsx
+++ src/chat/Chat.jsx
@@ -4,7 +4,7 @@ import ChatRoom from "./ChatRoom";
 
 export default function Chat() {
   return (
-    <div class="w3-container w3-padding-32 w3-center w3-black" >
+    <div class="w3-container w3-padding-32 w3-center w3-black margin-left: 200px" >
       <h1>Chat</h1>
       <hr />
 
--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -11,18 +11,14 @@ export default function Home() {
         <img src="https://indiehoy.com/wp-content/uploads/2020/09/nirvana-logo.jpg" alt="boy" class="w3-image" width="496" height="554" />
       </header>
       
-      <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="about">
-      <h2 class="w3-text-light-grey">My Name</h2>
+      <div class="w3-padding-large w3-content w3-justify w3-text-grey  w3-black" id="about">
+        <h2 class="w3-text-light-grey">My Name</h2>
     
         <p>Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
           ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
           adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </div>
-    
-
-      
-
 
     </div>
 
