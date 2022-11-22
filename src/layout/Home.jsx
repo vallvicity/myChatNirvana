f940cd642c25bf0c26a7d60bfaee031ec676b10a
Albert Ramos <vallvicity@hotmail.com>
2022-11-22 13:16:03 +0100

    Quinto commit

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -5,19 +5,20 @@ export default function Home() {
 
     <div class="w3-padding-large" id="main">
       <header class="w3-container w3-padding-32 w3-center w3-black" id="home">
-        <h1 class="w3-jumbo"><span class="w3-hide-small">Welcome to</span> Nirvana Tribute Page.</h1>
-        <p>I'm so ugly, that's ok 'cause so are you.</p>
-        <p>Broke our mirrors.</p>
+        <h1 class="w3-jumbo"><span class="w3-hide-small">I'm</span> Kurt Co.</h1>
+        <p>Singer, como la máquina de coser.</p>
         <img src="https://indiehoy.com/wp-content/uploads/2020/09/nirvana-logo.jpg" alt="boy" class="w3-image" width="496" height="554" />
       </header>
       
       <div class="w3-container w3-padding-64 w3-center w3- margin-top 200px w3-black" id="about">
         <h2 class="w3-text-light-grey">My Name</h2>
     
-        <p>Some text about me. Some text about me. I am lorem ipsum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-          ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        <p>PAra quien no me conozca, soy creador, artista musical y altavoz de la sociedad que me escuchaba. Creé un movimiento musical denominado grunge; basado en artistas independientes. Con poco esfuerzo y gracias a los circuitos main stream de la época, podrás aún tararear mis canciones.. 
+      Recuerdas el unplagged in NY? jijiji. Tuve seguidores en todas partes del mundo y ahora, gracias a Albertx2 y Ruben puedo conmtinuar debatiendo contigo acerca de ello.</p>
+        <p>La primera respuesta a tu pregunta ( que te la ahorro), es Sí, ella lo hizo. A partir de aqui puedes preguntarme lo que quieras acerca de drogas, música, cómo ser un buen padre y cómo desarrolar aplicaciones Android con Flutter. 
+      Sobre todo para que entiendas los conceptos correctamente, tienes que tener la mente abierta. Sin este requisito te quedarás colgado. Bueno, si has llegado hasta aquí tírate a la piscina, en pelotas como el niño aquél y empecemos a chatear.
         </p>
+        
       </div>
 
     </div>
